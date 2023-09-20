8b00a8ea96f30eb6fa404c80373b1a5d76547583
EthanJLindsey <99507491+EthanJLindsey@users.noreply.github.com>
2023-09-20 00:51:27 -0500

    synced amplify remote with github

--- amplify/backend/function/mywebsite3e407dd6/src/index.js
+++ amplify/backend/function/mywebsite3e407dd6/src/index.js
@@ -3,12 +3,41 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = event => {
+ 
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+const ses = new SESClient({ region: "us-east-2" });
+
+export const handler = async(event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
   for (const record of event.Records) {
     console.log(record.eventID);
     console.log(record.eventName);
     console.log('DynamoDB Record: %j', record.dynamodb);
+    const command = new SendEmailCommand({
+      Destination: {
+        ToAddresses: ["ethan.james.lindsey@gmail.com"],
+      },
+      Message: {
+        Body: {
+          Text: { Data: `${record.dynamodb.NewImage.content.S}`},
+        },
+  
+        Subject: { Data: "Message from Portfolio Website" },
+      },
+      Source: "noreply@mail.ethandev.net",
+    });
+
+    try {
+      let response = await ses.send(command);
+      // process data.
+      return response;
+    }
+    catch (error) {
+      // error handling.
+    }
+    finally {
+      // finally.
+    }
   }
   return Promise.resolve('Successfully processed DynamoDB record');
 };
