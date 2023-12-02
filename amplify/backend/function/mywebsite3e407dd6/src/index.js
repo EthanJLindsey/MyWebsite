892b00e9802b62049c260ff6d1073998d07dff63
Ethan Lindsey <99507491+EthanJLindsey@users.noreply.github.com>
2023-12-02 12:35:01 -0600

    uncommented code for emailing message

--- amplify/backend/function/mywebsite3e407dd6/src/index.js
+++ amplify/backend/function/mywebsite3e407dd6/src/index.js
@@ -1,43 +1,39 @@
-
-
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
- 
+
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 const ses = new SESClient({ region: "us-east-2" });
 
-export const handler = async(event) => {
+export const handler = async (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
   for (const record of event.Records) {
     console.log(record.eventID);
     console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
-    // const command = new SendEmailCommand({
-    //   Destination: {
-    //     ToAddresses: ["ethan.james.lindsey@gmail.com"],
-    //   },
-    //   Message: {
-    //     Body: {
-    //       Text: { Data: `${record.dynamodb.NewImage.content.S}`},
-    //     },
-  
-    //     Subject: { Data: "Message from Portfolio Website" },
-    //   },
-    //   Source: "noreply@mail.ethandev.net",
-    // });
+    console.log("DynamoDB Record: %j", record.dynamodb);
+    const command = new SendEmailCommand({
+      Destination: {
+        ToAddresses: ["ethan.james.lindsey@gmail.com"],
+      },
+      Message: {
+        Body: {
+          Text: { Data: `${record.dynamodb.NewImage.content.S}` },
+        },
 
-    // try {
-    //   let response = await ses.send(command);
-    //   // process data.
-    //   return response;
-    // }
-    // catch (error) {
-    //   // error handling.
-    // }
-    // finally {
-    //   // finally.
-    // }
+        Subject: { Data: "Message from Portfolio Website" },
+      },
+      Source: "noreply@mail.ethandev.net",
+    });
+
+    try {
+      let response = await ses.send(command);
+      // process data.
+      return response;
+    } catch (error) {
+      // error handling.
+    } finally {
+      // finally.
+    }
   }
-  return Promise.resolve('Successfully processed DynamoDB record');
+  return Promise.resolve("Successfully processed DynamoDB record");
 };
